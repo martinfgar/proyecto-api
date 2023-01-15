54e801a7aa590e7da82f224459a45cfff9d1748e
martinfgar <martinfifa2@gmail.com>
2023-01-15 15:53:21 +0100

    force json

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,9 +14,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if ($request->is('api/*')){
-            abort(401,'Check the required headers');
-        }
         if (! $request->expectsJson()) {
             return route('login');
         }
